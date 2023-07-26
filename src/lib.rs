b78a35e9f4c05ce0aae53efc3dcb803a5caef514
François Garillot <francois@garillot.net>
2023-07-26 13:06:06 -0400

    chore: remove groth16 left-overs
    - Deleted redundant utility function `le_bytes_to_u64s` from the main library.
    - Eliminated extraneous `groth16` feature condition check.

--- src/lib.rs
+++ src/lib.rs
@@ -30,16 +30,3 @@ mod constraint_system;
 pub use constraint_system::{Circuit, ConstraintSystem, Namespace, SynthesisError};
 
 pub const BELLPEPPER_VERSION: &str = env!("CARGO_PKG_VERSION");
-
-#[cfg(feature = "groth16")]
-pub(crate) fn le_bytes_to_u64s(le_bytes: &[u8]) -> Vec<u64> {
-    assert_eq!(
-        le_bytes.len() % 8,
-        0,
-        "length must be divisible by u64 byte length (8-bytes)"
-    );
-    le_bytes
-        .chunks(8)
-        .map(|chunk| u64::from_le_bytes(chunk.try_into().unwrap()))
-        .collect()
-}
